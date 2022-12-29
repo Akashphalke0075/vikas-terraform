5582b32fd17fb66db191d0e7352ff47666f3e3c1
Akash <phalke.aka@gmail.com>
2022-12-29 18:41:13 +0530

    update

--- version.tf
+++ version.tf
@@ -1,11 +1,9 @@
 terraform {
-  required_version = ">= 1.2.0"
-}
-terraform {
+  required_version = ">=1.2.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "4.48.0"
+        source = "hashicorp/aws"
+        version = ">=4.16.0"
     }
   }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "aws-us-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.10.80.0/22"
 }
 
 resource "aws_vpc" "aws-eu-vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.10.88.0/22"
   provider = aws.aws-eu-region
 }
