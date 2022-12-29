d772cfd519a47e6a8a21c2f252678830c552d210
Akash <phalke.aka@gmail.com>
2022-12-29 19:37:29 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "class-server" {
   ami           = "ami-0c2254f22b03751fa"
   instance_type = "t2.micro"
-
+  count = 3
   tags = {
-    Name = "HelloWorld"
+    Name = "class-server-${count.index}"
   }
 }
--- elastic.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_eip" "class-server" {
-  instance = aws_instance.web.id
-  depend_on = [aws_eip.aws_instance.web]
-}
